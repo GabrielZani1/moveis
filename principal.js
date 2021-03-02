073de069d82623c3a4fad83fb542ff9b10c7b37e
Gabriel Zani <gabriel@eficienciafiscal.com.br>
2021-03-02 08:38:17 -0400

    Diminuindo intervalo de troca de banner

--- principal.js
+++ principal.js
@@ -2,6 +2,7 @@ var banners = ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
 var bannerAtual = 0;
 
 function trocaBanner(){
+	setInterval(trocaBanner, 1000);
 	BannerAtual = (bannerAtual + 1) % 2;
 	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
 }
